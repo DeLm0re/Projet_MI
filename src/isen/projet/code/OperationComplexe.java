c7cc5a957ff0975ae2af7b18de848d49868d7457
onceuponaban <onceuponaban@gmail.com>
2017-12-19 18:29:07 +0100

    Surcharge de l'addition, soustraction et multiplication pour pouvoir calculer avec un nombre complexe et un réel.

--- src/isen/projet/code/OperationComplexe.java
+++ src/isen/projet/code/OperationComplexe.java
@@ -26,11 +26,35 @@ public final class OperationComplexe
         return zs;
     }
 
+    public static NombreComplexe additionner(NombreComplexe z1, double x)
+    {
+        NombreComplexe zs = new NombreComplexe();
+        zs.setPartieReelle(x+z1.getPartieReelle());
+        zs.setPartieImaginaire(z1.getPartieImaginaire());
+        return zs;
+    }
+
     public static NombreComplexe soustraire(NombreComplexe z1, NombreComplexe z2)
     {
         NombreComplexe zs = new NombreComplexe();
         zs.setPartieReelle(z1.getPartieReelle()-z2.getPartieReelle());
         zs.setPartieImaginaire(z1.getPartieImaginaire()-z2.getPartieImaginaire());
+        return zs;
+    }
+
+    public static NombreComplexe soustraire(NombreComplexe z1, double x)
+    {
+        NombreComplexe zs = new NombreComplexe();
+        zs.setPartieReelle(z1.getPartieReelle()-x);
+        zs.setPartieImaginaire(z1.getPartieImaginaire());
+        return zs;
+    }
+
+    public static NombreComplexe soustraire(double x, NombreComplexe z1)
+    {
+        NombreComplexe zs = new NombreComplexe();
+        zs.setPartieReelle(x-z1.getPartieReelle());
+        zs.setPartieImaginaire(z1.getPartieImaginaire());
         return zs;
     }
 
@@ -45,6 +69,14 @@ public final class OperationComplexe
         NombreComplexe zs = new NombreComplexe();
         zs.setPartieReelle(z1.getPartieReelle()*z2.getPartieReelle()-z1.getPartieImaginaire()*z2.getPartieImaginaire());
         zs.setPartieImaginaire(z1.getPartieReelle()*z2.getPartieImaginaire()+z1.getPartieImaginaire()*z2.getPartieReelle());
+        return zs;
+    }
+
+    public static NombreComplexe multiplier(NombreComplexe z1, double x)
+    {
+        NombreComplexe zs = new NombreComplexe();
+        zs.setPartieReelle(x*z1.getPartieReelle());
+        zs.setPartieImaginaire(x*z1.getPartieImaginaire());
         return zs;
     }
 
