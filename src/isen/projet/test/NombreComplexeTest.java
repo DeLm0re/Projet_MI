cbae7f2d9b94fc0d6b2bdb553f423b49e54f9964
Parazar <theo.hipault@gmail.com>
2017-12-19 11:27:05 +0100

    Base des tests
    Mise en place de l'architechture des test JUnit pour chaque classe

---
+++ src/isen/projet/test/NombreComplexeTest.java
@@ -0,0 +1,37 @@
+package isen.projet.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NombreComplexeTest {
+    @Test
+    void conjugueTest()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void moduleTest()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void argumentTest()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void toStringTest()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void formeExponentielleTest()
+    {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/isen/projet/test/OperationComplexeTest.java
@@ -0,0 +1,31 @@
+package isen.projet.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationComplexeTest {
+    @Test
+    void inverseTest()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void additionnerTest()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void multiplierTest()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void diviserTest()
+    {
+        fail("Not yet implemented");
+    }
+
+}
