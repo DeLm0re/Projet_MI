a46b25f1112270e8f0abb0121f1a4448aa63ed71
onceuponaban <onceuponaban@gmail.com>
2017-12-19 11:47:30 +0100

    Renommage de partieEntiere à partieReelle
    Ce nom là est beaucoup plus logique.

--- src/isen/projet/code/NombreComplexe.java
+++ src/isen/projet/code/NombreComplexe.java
@@ -7,27 +7,27 @@ import static java.lang.Math.*;
 
 public class NombreComplexe
 {
-    private double partieEntiere;
+    private double partieReelle;
     private double partieImaginaire;
 
-    NombreComplexe(double partieEntiere, double partieImaginaire)
+    NombreComplexe(double partieReelle, double partieImaginaire)
     {
-        this.partieEntiere=partieEntiere;
+        this.partieReelle=partieReelle;
         this.partieImaginaire=partieImaginaire;
     }
 
     NombreComplexe()
     {
-        this.partieEntiere=0;
+        this.partieReelle=0;
         this.partieImaginaire=0;
     }
 
-    public double getPartieEntiere() {
-        return partieEntiere;
+    public double getPartieReelle() {
+        return partieReelle;
     }
 
-    public void setPartieEntiere(double partieEntiere) {
-        this.partieEntiere = partieEntiere;
+    public void setpartieReelle(double partieReelle) {
+        this.partieReelle = partieReelle;
     }
 
     public double getPartieImaginaire() {
@@ -40,27 +40,27 @@ public class NombreComplexe
 
     public void setNombreComplexe(NombreComplexe z)
     {
-        this.partieEntiere = z.getPartieEntiere();
+        this.partieReelle = z.getPartieReelle();
         this.partieImaginaire = z.getPartieImaginaire();
     }
 
     public NombreComplexe conjugue()
     {
-        NombreComplexe zs = new NombreComplexe(partieEntiere,-partieImaginaire);
+        NombreComplexe zs = new NombreComplexe(partieReelle,-partieImaginaire);
         return zs;
     }
 
     public double module()
     {
-        return sqrt(partieEntiere*partieEntiere + partieImaginaire*partieImaginaire);
+        return sqrt(partieReelle*partieReelle + partieImaginaire*partieImaginaire);
     }
 
     public double argument()
     {
-        if(partieEntiere<0 && partieImaginaire==0)
+        if(partieReelle<0 && partieImaginaire==0)
             return PI;
         else
-            return 2*atan(partieImaginaire/(partieEntiere + module()));
+            return 2*atan(partieImaginaire/(partieReelle + module()));
     }
 
     public String toString()
